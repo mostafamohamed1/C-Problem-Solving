46bdb1f34f1de822319db1dddba3dcb147e4db7a
mostafamohamed1 <mostafa.mohamed2700@gmail.com>
2024-01-14 13:48:41 +0200

    Some Kata

---
+++ 29-Is-it-a-number/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+
+bool is_digit(std::string s)
+{
+
+  if (s.size() == 0)
+    return false;
+
+  size_t indexDot = s.find('.');
+  if (indexDot < s.size())
+    s.erase(indexDot, 1);
+  if (s.at(0) == '-')
+    s.erase(0, 1);
+
+  return std::all_of(s.begin(), s.end(), ::isdigit);
+}
+
+int main()
+{
+
+  std::cout << is_digit("35.65") << std::endl;
+  std::cout << is_digit("s2324") << std::endl;
+  std::cout << is_digit("3 4") << std::endl;
+  std::cout << is_digit("-234.4") << std::endl;
+  return 0;
+}
